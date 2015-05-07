c284b29f9e2752699343c453a59bf24a73ded27d
Matthew Russell <mattlruss@gmail.com>
2015-05-07 14:35:14 -0500

    added potential claim function to triggers.py
    inputs: a trigger index, the sender, optional: character and game
    output: removes trigger from list and adds to used triggers list with extra specified data, or fails with exception
    
    problems:
    no idea how to remove or add data with regards to the google API.
    referencing the sheets might have been done wrong, can someone doublecheck?

--- triggers.py
+++ triggers.py
@@ -25,3 +25,21 @@ class TriggerSheet:
             raise IndexError('bad event number')
         
         return "(%d) %s" % (num, self.events[num-1])
+
+def claim(self, triggerIndex, sender, character = "", game = ""):
+	# character and game are option inputs
+	
+	triggerSheet = self.sheet.sheet1
+	usedSheet = self.sheet.sheet2
+
+	# trust the exception handler if trigger index is out of bounds
+	trigger = triggerSheet.col_values(1)[triggerIndex-1]
+	writer = triggerSheet.col_values(2)[triggerIndex-1]
+	if trigger is None or writer is None:
+		raise IndexError('no trigger found at [%s]' % triggerIndex)
+
+	# I have no idea how to add this to the spreadsheet, this is just 
+	# for general formatting purposes
+	usedSheet.pushRow([game, trigger, writer, character, sender])
+	# again, no idea how to do this remove rows
+	triggerSheet.popRow(triggerIndex-1)
