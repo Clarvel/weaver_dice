2f7f53d539504c4c6e58180c15e02b60c1ba0a07
Matthew Russell <mattlruss@gmail.com>
2015-05-07 13:03:13 -0500

    Update triggers.py
    hopeful fix for trigger 0 edge case
    random trigger should now only include actual triggers, no more "None" triggers.

--- triggers.py
+++ triggers.py
@@ -20,8 +20,19 @@ class TriggerSheet:
         assert wks.title == "Trigger Events"
         return wks
     def event(self, num=None):
-        if not num: num = random.randint(1,len(self.events)+1)
-        if num not in range(1,len(self.events)+1):
+        length = len(self.events)
+
+        # if no number specified, choose randomly from available triggers
+        if num is None: 
+            # determine which trigger indexes are filled
+            triggers = []
+            for a in range(1, length):
+                if self.events[a-1] is not None:
+                    triggers.push(a)
+            # choose one index randomly
+            num = triggers[random.randint(0,len(triggers))]
+
+        # if num is not in the possible indexes, raise error
+        if num not in range(1,length):
             raise IndexError('bad event number')
-        
         return "(%d) %s" % (num, self.events[num-1])
